e7f3f86957d627d3698819fb5f7d16227813dd8e
Aditya Gusain <adisandi3112002@gmail.com>
2021-09-24 20:57:19 +0530

    Created Carousal and Cards for Nutrition page

--- client/src/Components/temp.jsx
+++ client/src/Components/temp.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import Brand from "./Delivery/Brand";
 import Delivery from "./Delivery/index";
+import Dining from "./Dining";
+import NightLife from "./NightLife";
+import Nutrition from "./Nutrition";
 
 function Temp() {
   const { type } = useParams();
@@ -10,6 +13,9 @@ function Temp() {
       <div className="my-5">
         {type === "delivery" && <Brand />}
         {type === "delivery" && <Delivery />}
+        {type === "dining" && <Dining />}
+        {type === "night" && <NightLife />}
+        {type === "nutri" && <Nutrition />}
       </div>
     </>
   );
