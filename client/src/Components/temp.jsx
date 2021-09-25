59bbedfef0c7332894deb9daff5885af198e9f22
Aditya Gusain <adisandi3112002@gmail.com>
2021-09-25 21:19:43 +0530

    Restaurant Image Grid and Restaurant Details 25%

--- client/src/Components/temp.jsx
+++ client/src/Components/temp.jsx
@@ -1,22 +1,9 @@
 import React from "react";
-import { useParams } from "react-router-dom";
-import Brand from "./Delivery/Brand";
-import Delivery from "./Delivery/index";
-import Dining from "./Dining";
-import NightLife from "./NightLife";
-import Nutrition from "./Nutrition";
 
 function Temp() {
-  const { type } = useParams();
   return (
     <>
-      <div className="my-5">
-        {type === "delivery" && <Brand />}
-        {type === "delivery" && <Delivery />}
-        {type === "dining" && <Dining />}
-        {type === "night" && <NightLife />}
-        {type === "nutri" && <Nutrition />}
-      </div>
+      <div className="my-5">Temp Page</div>
     </>
   );
 }
