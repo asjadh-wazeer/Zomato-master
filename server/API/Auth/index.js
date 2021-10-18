1a969f20b01a3e267ba844f06c9102567c622ad9
Aditya Gusain <adisandi3112002@gmail.com>
2021-10-18 21:44:02 +0530

    ...

--- server/API/Auth/index.js
+++ server/API/Auth/index.js
@@ -18,15 +18,15 @@ Access          Public
 Method          POST
 */
 Router.post("/signup", async (req, res) => {
-    try {
-        await ValidateSignup(req.body.credentials);
-        await UserModel.findByEmailAndPhone(req.body.credentials);
-        const newUser = await UserModel.create(req.body.credentials);
-        const token = newUser.generateJwtToken();
-        return res.status(200).json({ token, status: "success" });
-    } catch (error) {
-        return res.status(500).json({ error: error.message });
-    }
+  try {
+    await ValidateSignup(req.body.credentials);
+    await UserModel.findByEmailAndPhone(req.body.credentials);
+    const newUser = await UserModel.create(req.body.credentials);
+    const token = newUser.generateJwtToken();
+    return res.status(200).json({ token, status: "success" });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 });
 
 /*
@@ -37,16 +37,14 @@ Access          Public
 Method          POST
 */
 Router.post("/signin", async (req, res) => {
-    try {
-        await ValidateSignin(req.body.credentials);
-        const user = await UserModel.findByEmailAndPassword(
-            req.body.credentials
-        );
-        const token = user.generateJwtToken();
-        return res.status(200).json({ token, status: "success" });
-    } catch (error) {
-        return res.status(500).json({ error: error.message });
-    }
+  try {
+    await ValidateSignin(req.body.credentials);
+    const user = await UserModel.findByEmailAndPassword(req.body.credentials);
+    const token = user.generateJwtToken();
+    return res.status(200).json({ token, status: "success" });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 });
 
 /*
@@ -57,13 +55,13 @@ Access          Public
 Method          GET
 */
 Router.get(
-    "/google",
-    passport.authenticate("google", {
-        scope: [
-            "https://www.googleapis.com/auth/userinfo.profile",
-            "https://www.googleapis.com/auth/userinfo.email",
-        ],
-    })
+  "/google",
+  passport.authenticate("google", {
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.profile",
+      "https://www.googleapis.com/auth/userinfo.email",
+    ],
+  })
 );
 
 /*
@@ -74,11 +72,13 @@ Access          Public
 Method          GET
 */
 Router.get(
-    "/google/callback",
-    passport.authenticate("google", { failureRedirect: "/" }),
-    (req, res) => {
-        return res.json({ token: req.session.passport.user.token });
-    }
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    return res.redirect(
+      `http://localhost:3000/google/${req.session.passport.user.token}`
+    );
+  }
 );
 
 export default Router;
